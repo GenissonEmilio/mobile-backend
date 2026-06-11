0b1d19f22312524020f46a9ae5927161ba5f8c3e
Genisson Emilio <esgenisson@gmail.com>
2026-06-11 10:59:56 -0300

    chore: remove dist folder from version control and update gitignore

--- dist/services/productService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productService = void 0;
-exports.productService = {
-// Métodos de produtos futuramente
-};
-//# sourceMappingURL=productService.js.map
